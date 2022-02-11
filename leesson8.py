0ab1c36f577b608b1e52bd36a866bf3e1149d12e
Putya19 <99470781+Putya19@users.noreply.github.com>
2022-02-11 11:20:03 +0300

    Add files via upload

---
+++ leesson8.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+try:
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    first_name = browser.find_element(By.CSS_SELECTOR,".form-control.first:required").send_keys("Iron")
+    last_name = browser.find_element(By.CSS_SELECTOR, "form-control.second:required").send_keys("Man")
+    email = browser.find_element(By.CSS_SELECTOR, ".form-control.third").send_keys("marvel@mail.com")
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
